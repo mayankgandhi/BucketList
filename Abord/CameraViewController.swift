dfcf5c4b79ad47620bc524d915f4b1244c279812
Mayank Gandhi <mayankgandhi50@gmail.com>
2020-03-31 01:02:43 +0530

    somewhat stable build

---
+++ Abord/CameraViewController.swift
@@ -0,0 +1,61 @@
+//
+//  CameraViewController.swift
+//  Abord
+//
+//  Created by Mayank on 31/03/20.
+//  Copyright © 2020 Mayank. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func submitPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func onCameraPressed(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera)   {
+            picker.sourceType = .camera
+        }   else    {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        imageView.image = image
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Abord' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Abord
+
+ pod 'Parse'
+pod 'AlamofireImage', '~> 4.0'
+
+  target 'AbordTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'AbordUITests' do
+    # Pods for testing
+  end
+
+end
