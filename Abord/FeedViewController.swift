90a6f2fcbd6220c6a4317ad8f05ab4d9d959899f
Mayank Gandhi <mayankgandhi50@gmail.com>
2020-03-31 15:03:33 +0530

    working prototype

--- Abord/FeedViewController.swift
+++ Abord/FeedViewController.swift
@@ -7,15 +7,73 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    var posts = [PFObject]()
+    
 
+    @IBOutlet weak var postTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        postTableView.delegate = self
+        postTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        //For Example, if you only want to query the last 20
+        query.limit = 20
+        query.findObjectsInBackground { (posts,Error) in
+            if posts != nil{
+                self.posts = posts!
+                self.postTableView.reloadData()
+            }   else    {
+                print(Error?.localizedDescription)
+            }
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableCell = postTableView.dequeueReusableCell(withIdentifier: "PostTableViewCell") as! PostTableViewCell
+        let post = posts[indexPath.row]
+        
+        let user = post["author"] as! PFUser
+        let caption = post["caption"] as! String
+        tableCell.usernameLabel.text = user.username
+        tableCell.CaptionLabel.text = caption
+        
+//        let userImageFile = post["image"] as! PFFileObject
+//        userImageFile.getDataInBackground { (imageData: Data?, error: Error?) in
+//            if let error = error {
+//                print(error.localizedDescription)
+//            } else if let imageData = imageData {
+//                print("image found!")
+//                let image = UIImage(data:imageData)
+//                tableCell.postPhotoView.image = image
+//            }
+//        }
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let imageURL = URL(string: urlString)!
+        print(imageURL)
+        tableCell.postPhotoView.af_setImage(withURL: imageURL)
+        
+        return tableCell
+    }
     
     /*
     // MARK: - Navigation
---
+++ Abord/PostTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostTableViewCell.swift
+//  Abord
+//
+//  Created by Mayank on 31/03/20.
+//  Copyright © 2020 Mayank. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var postPhotoView: UIImageView!
+    @IBOutlet weak var CaptionLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
