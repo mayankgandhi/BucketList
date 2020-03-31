9b455e66f2149c6b39e9c2c7e45a7c568e5e8145
Mayank Gandhi <mayankgandhi50@gmail.com>
2020-03-31 23:29:39 +0530

    working reverse-geolocation

---
+++ Abord/MapViewController.swift
@@ -0,0 +1,130 @@
+//
+//  MapViewController.swift
+//  Abord
+//
+//  Created by Mayank on 31/03/20.
+//  Copyright © 2020 Mayank. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var addressLabel: UILabel!
+    let locationManager = CLLocationManager()
+    let regionInMeters:Double = 10000
+    var previousLocation:CLLocation?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        // Do any additional setup after loading the view.
+        checkLocationServices()
+    }
+    
+    func setupLocationServices()    {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func checkLocationServices()    {
+        if CLLocationManager.locationServicesEnabled()  {
+            //if Location Services enabled, setup Location Manager
+            setupLocationServices()
+            checkLocationAuthorisation()
+        }   else    {
+            
+            
+        }
+    }
+    
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func checkLocationAuthorisation()   {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways:
+           break
+        case .authorizedWhenInUse:
+            startTrackingUserLocation()
+        case .denied:
+            break
+        case .restricted:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func startTrackingUserLocation()    {
+        mapView.showsUserLocation = true
+        centerViewOnUserLocation()
+        locationManager.startUpdatingLocation()
+        previousLocation = getCenterLocation(for: mapView)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func getCenterLocation( for mapView:MKMapView)  -> CLLocation   {
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+    
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
+
+}
+
+extension MapViewController:CLLocationManagerDelegate   {
+//  Keep following the current location as the center of the screen
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        guard let location = locations.last else { return }
+//        let region = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+//        mapView.setRegion(region, animated: true)
+//    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorisation()
+    }
+}
+
+extension MapViewController:MKMapViewDelegate   {
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        let geoCoder = CLGeocoder()
+        guard let previousLocation = self.previousLocation else { return }
+        guard center.distance(from: previousLocation) > 50 else { return }
+        self.previousLocation=center
+        geoCoder.reverseGeocodeLocation(center) { [weak self] ( placemarks, Error) in
+            guard let self = self else { return }
+            if let error = Error {
+                return
+            }
+            guard let placemark = placemarks?.first else     {
+                return
+                
+            }
+            let region = placemark.locality ?? " "
+            let country = placemark.country ?? " "
+            DispatchQueue.main.async {
+                self.addressLabel.text = "\(region),\(country)"
+            }
+        }
+        
+        
+    }
+}
